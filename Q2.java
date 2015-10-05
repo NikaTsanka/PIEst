25653602431e50f54bd23e6e20eb2dabcdb575f8
NikaTsanka <nikatsanka@outlook.com>
2015-10-05 16:53:10 -0400

    First commit for Q2

--- Q2.java
+++ Q2.java
@@ -1,8 +1,18 @@
 
+
 public class Q2 {
+	
+	private final static double RADIUS = 1.0;
+	private static double areaOfCircle = 0;
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Area of the top right quadrant of the circle.
+		areaOfCircle = (Math.PI * Math.pow(RADIUS, 2.0)) / 4;
+
+		
+		System.out.println(areaOfCircle);
 
 	}
 
