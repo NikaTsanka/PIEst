7db6bd38bc3b47bd8d8c0818de4895a7a7f09755
NikaTsanka <nikatsanka@outlook.com>
2015-10-07 15:32:41 -0400

    Final Commit. Wednesday - 10/07/15 - 3:32PM

--- Q2.java
+++ Q2.java
@@ -1,19 +1,88 @@
+import java.math.*;
+import java.util.Random;
+import static java.lang.System.out;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
-
+/*
+ * PI estimation simulation. AKA "Monte Carlo method"
+ */
 public class Q2 {
-	
-	private final static double RADIUS = 1.0;
-	private static double areaOfCircle = 0;
+	//Random Variable.
+	private static Random rnd;
 
+	//BigInteger Variables.
+	private static BigInteger INARC;
+	private static BigInteger bigNum;
+	private static BigInteger i;
+
+	//BigDecimal Variables.
+	private static BigDecimal INARC0;
+	private static BigDecimal bigNum0;
+	private static BigDecimal FOUR;
+	private static BigDecimal result;
+
+	//String Variables.
+	private static String INARCToString;
+	private static String bigNumToString;
+
+	//Double Variables - Initialized.
+	private static double x = 0, y = 0, rNum = 0;
 
 	public static void main(String[] args) {
-		
-		//Area of the top right quadrant of the circle.
-		areaOfCircle = (Math.PI * Math.pow(RADIUS, 2.0)) / 4;
+		//Timer
+		long startTime = System.nanoTime();
 
-		
-		System.out.println(areaOfCircle);
+		//Initialize Random. Range will be 0 to 1; 0 inclusive.
+		rnd = new Random();
 
+		//Initialize BigIntegers.
+		INARC = BigInteger.ZERO;
+		bigNum = new BigInteger("4000000000");
+		i = BigInteger.ZERO;
+
+		while ((i.compareTo(bigNum) == -1)) {
+
+			x = rnd.nextDouble();
+			y = rnd.nextDouble();
+
+			rNum = ((x * x) + (y * y));
+
+			/*
+			 * Didn't notice that BigInteger and BigDecimal
+			 * were immutable even though it clearly said in
+			 * the book. So I was doing something like
+			 * INARC.add(BigInteger.ONE) and it didn't work.
+			 * But now I know. :)
+			 */
+			if (rNum <= 1.0) {
+				INARC = INARC.add(BigInteger.ONE);
+			}
+			i = i.add(BigInteger.ONE);
+
+		}
+		//Set Strings.
+		INARCToString = INARC.toString();
+		bigNumToString = bigNum.toString();
+
+		//Set BigDecimals.
+		INARC0 = new BigDecimal(INARCToString);
+		bigNum0 = new BigDecimal(bigNumToString);
+		FOUR = new BigDecimal("4.0");
+
+		//Calculate. 10 digits after the decimal point should be good.
+		result = INARC0.multiply(FOUR).divide(bigNum0, 10, 
+				BigDecimal.ROUND_UP);
+
+		out.println("PI = " + result);
+
+		//Timer
+		long estimatedTime = System.nanoTime() - startTime;
+
+		//I knew how to get the time but this way is better. :)
+		//http://stackoverflow.com/questions/924208/how-to-convert-nanoseconds-to-seconds-using-the-timeunit-enum
+		out.println("Estimation time: " +
+				NANOSECONDS.toMinutes(estimatedTime) + " minutes and " + 
+				(NANOSECONDS.toMinutes(estimatedTime) % 60) + " seconds." );
 	}
 
 }
